acab25ac6c328012148fcc9e3c3f8ebd03a4fc75
Mehmet Tuzcu <mehmettuzcu22@gmail.com>
2023-12-19 20:24:08 +0000

    wip

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ terraform {
   #    name = "terra-house-1"
   #  }
   #}
-  #cloud {
-  #  organization = "ExamPro"
-  #  workspaces {
-  #    name = "terra-house-1"
-  #  }
-  #}
+  cloud {
+    organization = "mehmettuzcu22"
+    workspaces {
+      name = "terraform-cloud"
+    }
+  }
 
 }
 
